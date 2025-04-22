1033f426247a54b979b0137eed6878dcd6e0ad24
Alex X <dowsha@yandex.ru>
2025-04-22 19:25:38 +0300

    Обновил основной urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # Добавление Swagger документации
+    path('', include('api.urls')),
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger'),
+    path('swagger/', 
+         SpectacularSwaggerView.as_view(url_name='schema'), name='swagger'),
 ]
